7d840cdb137ebabfd991f89faed503f24811147a
AlanBaron <alanelbaron@gmail.com>
2019-05-10 16:33:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pyaml
 from rdflib.graph import Graph
 import pprint
 from pathlib import Path
+import urllib
 
 def Consistency_subClassesProperties() :
 	liste_S = []
@@ -14,7 +15,6 @@ def Consistency_subClassesProperties() :
 	liste_Pd = []
 	points = 0
 	nbPossible = 0
-	a = 0
 	for s, _, o in g.triples((None, rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'), None)) : #Si jamais y'a pas de subClassOf, on entre pas ici
 		liste_S.append(s)
 		liste_O.append(o)
@@ -25,19 +25,19 @@ def Consistency_subClassesProperties() :
 		for x in liste_S :
 			for y in liste_O :
 				if x == y: #On a un objet qui est aussi sujet, avec subClassOf en lien. Pas besoin de vérifier cas x vide
-				#Cas numéro 1 : transitivité subClassOf
+				#Cas numéro 1 : transitivité subClassOf (doit-on vraiment vérifier ça? Le cas 2 couvre ce cas normalement, en plus simple et plus optimiser... Y réfléchir)
 					for s1, _, _ in g.triples((None, rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'), x)) :
 						for _, _, o1 in g.triples((x, rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'), None)) :
 							nbPossible = nbPossible + 1
 							for _, p1, _ in g.triples((s1, None, o1)) :    
 								if p1 == rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf') :
-									a = a+1
+									points = points + 1
 				#Cas numéro 2 : A a B, B subClassOf C, A a C
 				for s2, p2, _ in g.triples((None, None, x)) :
 					for s3, _, _ in g.triples((s2, p2, y)) :
 						nbPossible = nbPossible + 1;
 						if s3 is not None :
-							a = a + 1
+							points = points + 1
 	#Ici les subPropertyOf						
 	if len(liste_Pg) != 0:
 		for i in range(0,len(liste_Pg)-1) :
@@ -45,13 +45,134 @@ def Consistency_subClassesProperties() :
 				for s5, _, _ in g.triples((s4, Pd[i], o4)) :
 					nbPossible = nbPossible + 1
 					if s5 is not None :
-						a = a + 1
+						points = points + 1
 						
 	if nbPossible == 0 :
 		return 0
 	else :
-		return a/nbPossible
+		return points/nbPossible
+		
+def Consistency_equivalentClassesProperties() :
+	liste_S = []
+	liste_O = []
+	liste_Pg = []
+	liste_Pd = []
+	points = 0
+	nbPossible = 0
+	for s, _, o in g.triples((None, rdflib.term.URIRef('https://www.w3.org/2002/07/owl#equivalentClass'), None)) :
+		liste_S.append(s)
+		liste_O.append(o)
+	for sp, _, so in g.triples((None, rdflib.term.URIRef('https://www.w3.org/2002/07/owl#equivalentProperty'), None)) :
+		liste_Pg.append(sp)
+		liste_Pd.append(so)
+	if len(liste_S) != 0 :	
+		for x in liste_S :
+			for y in liste_O :
+				for s2, p2, _ in g.triples((None, None, x)) :
+					for s3, _, _ in g.triples((s2, p2, y)) :
+						nbPossible = nbPossible + 1;
+						if s3 is not None :
+							points = points + 1
+	if len(liste_Pg) != 0:
+		for i in range(0,len(liste_Pg)-1) :
+			for s4, _, o4 in g.triples((None, Pg[i], None)) :
+				for s5, _, _ in g.triples((s4, Pd[i], o4)) :
+					nbPossible = nbPossible + 1
+					if s5 is not None :
+						points = points + 1
+						
+	if nbPossible == 0 :
+		return 0
+	else :
+		return points/nbPossible
+		
+def Consistency_disjointWith() :
+	liste_S = []
+	liste_O = []
+	points = 0
+	nbPossible = 0
+	for s, _, o in g.triples((None, rdflib.term.URIRef('https://www.w3.org/2002/07/owl#disjointWith'), None)) :
+		liste_S.append(s)
+		liste_O.append(o)
+	if len(liste_S) != 0 :	
+		for x in liste_S :
+			for y in liste_O :
+				for s1, _, _ in g.triples((None,  rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'), x)) :
+					for _, p2, _ in g.triples((s1, None, y)) :
+						nbPossible = nbPossible + 1
+						if p2 is not None and p2 != rdflib.term.URIRef('https://www.w3.org/2002/07/owl#disjointWith') :
+							points = points + 1
+					for _, p7, _ in g.triples((y, None, s1)) :
+						nbPossible = nbPossible + 1
+						if p7 is not None and p7 != rdflib.term.URIRef('https://www.w3.org/2002/07/owl#disjointWith') :
+							points = points + 1
+				for _, p3, _ in g.triples((x, None, y)):
+					nbPossible = nbPossible + 1
+					if p3 is not None and p3 != rdflib.term.URIRef('https://www.w3.org/2002/07/owl#disjointWith') :
+						points = points + 1
+				for _, p4, _ in g.triples((y, None, x)):
+					nbPossible = nbPossible + 1
+					if p4 is not None and p4 != rdflib.term.URIRef('https://www.w3.org/2002/07/owl#disjointWith') :
+						points = points + 1
+				for s5, _, _ in g.triples((None, rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'), y)) :
+					for _, p6, _ in g.triples((s5, None, x)) :
+						nbPossible = nbPossible + 1
+						if p6 is not None and p6 != rdflib.term.URIRef('https://www.w3.org/2002/07/owl#disjointWith') :
+							points = points + 1
+					for _, p8, _ in g.triples((x, None, s5)) :
+						nbPossible = nbPossible + 1
+						if p8 is not None and p8 != rdflib.term.URIRef('https://www.w3.org/2002/07/owl#disjointWith') :
+							points = points + 1
+	if nbPossible == 0 :
+		return 0
+	else :
+		return 0-(points/nbPossible)
 	
+def Conciseness_longURI() :
+	nbPossible = 0
+	points = 0
+	for s, p, o in g.triples((None, None, None)) :
+		if isinstance(s, rdflib.term.URIRef) :
+			nbPossible = nbPossible + 1
+			if len(p) >= 80 :
+				points = points + 1
+		if isinstance(p, rdflib.term.URIRef) :
+			nbPossible = nbPossible + 1
+			if len(p) >= 80 :
+				points = points + 1
+		if isinstance(o, rdflib.term.URIRef) :
+			nbPossible = nbPossible + 1
+			if len(p) >= 80 :
+				points = points + 1
+	print(nbPossible)
+	print(points)
+	if nbPossible == 0:
+		return 0
+	else :
+		return 0-(points/nbPossible)
+		
+def Availability_Error() :
+	nbPossible = 0
+	points = 0
+	for s, p, o in g.triples((None, None, None)) :
+		if isinstance(s, rdflib.term.URIRef) :
+			nbPossible = nbPossible + 1
+			a = urllib.urlopen(s).getcode()
+			if a >= 400 :
+				points = points + 1
+		if isinstance(p, rdflib.term.URIRef) :
+			nbPossible = nbPossible + 1
+			a = urllib.urlopen(p).getcode()
+			if a >= 400 :
+				points = points + 1
+		if isinstance(o, rdflib.term.URIRef) :
+			nbPossible = nbPossible + 1
+			a = urllib.urlopen(o).getcode()
+			if a >= 400 :
+				points = points + 1
+	
+	
+#Ceci est le main... Voir comment bien le faire avec python
 parser = argparse.ArgumentParser()
 parser.add_argument('file')
 args = parser.parse_args()
@@ -67,7 +188,7 @@ if os.path.exists(args.file)  :
 			pprint.pprint(s)
 			pprint.pprint(p)
 			pprint.pprint(o)
-		print(Consistency_subClassesProperties())
+		Conciseness_longURI()
 			
 	
 
