198a353d472a89363136acd8adc5c1224be517ea
AlanBaron <alanelbaron@gmail.com>
2019-05-13 16:21:33 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from rdflib.graph import Graph
 import pprint
 from pathlib import Path
 import urllib
+from SPARQLWrapper import SPARQLWrapper, XML
+
 
 def Consistency_subClassesProperties() :
 	liste_S = []
@@ -170,8 +172,55 @@ def Availability_Error() :
 			a = urllib.urlopen(o).getcode()
 			if a >= 400 :
 				points = points + 1
-	
-	
+				
+def Conciseness_duplicatedRules() :  #Oublier!!!
+	nbPossible = 0
+	points = 0
+	liste_spo = []
+	for s, p, o in g.triples((None, None, None)) :
+		liste_spo.append((s, p, o))
+	nbPossible = len(liste_spo)
+	if nbPossible == 0 :
+		return 0
+	else : 
+		return 0-(points/nbPossible)
+		
+def Consistency_domainRange() :
+	nbPossible = 0
+	points = 0
+	liste_O = []
+	for s, p, o in g.triples((None, None, None)) :
+		for _, _, o2 in g.triples((p or None, rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#range'), None)) :
+			nbPossible = nbPossible + 1
+			for _, _, o3 in g.triples((s, rdflib.term.URIRef('a'), None)) :
+				if o2 != o3 :
+					liste_O.append(o3)
+					for _, _, o4 in g.triples((o3, rdflib.term.URIRef('https://www.w3.org/2002/07/owl#equivalentClass'), None)) :
+						liste_O.append(o4)
+					for O in liste_O :
+						for _, _, o5 in g.triples((O, rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'), None)) : #On considère le graphe comme complet ici. Transitivité des subclassOf, ... Donc un mauvais schéma peut pottentiellement perdre beaucoup de points
+							if o2 != o5:
+								points = points + 1
+		liste_O = []			
+		for _, _, o2 in g.triples((p, rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#domain'), None))	:
+			nbPossible = nbPossible + 1
+			for _, _, o3 in ((o, rdflib.term.URIRef('a'), None)) :
+				if o2 != o3 :
+					liste_O.append(o3)
+					for _, _, o4 in g.triples((o3, rdflib.term.URIRef('https://www.w3.org/2002/07/owl#equivalentClass'), None)) :
+						liste_O.append(o4)
+					for O in liste_O :
+						for _, _, o5 in g.triples((O, rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'), None)) : #On considère le graphe comme complet ici. Transitivité des subclassOf, ... Donc un mauvais schéma peut pottentiellement perdre beaucoup de points
+							if o2 != o5:
+								points = points + 1
+	if nbPossible == 0 :
+		return 0
+	else :
+		return 0-(points/nbPossible)
+				
+			
+		
+		
 #Ceci est le main... Voir comment bien le faire avec python
 parser = argparse.ArgumentParser()
 parser.add_argument('file')
@@ -185,10 +234,7 @@ if os.path.exists(args.file)  :
 			g = Graph()
 			g.parse(file)
 		for s, p, o in g.triples((None, None, None)) :
-			pprint.pprint(s)
 			pprint.pprint(p)
-			pprint.pprint(o)
-		Conciseness_longURI()
+		print(Consistency_domainRange())
 			
 	
-
